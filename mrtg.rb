6c02ddfe35bb34799d636b800141cae704e847af
Nate Murray <nate@natemurray.com>
2009-02-02 14:29:49 -0800

    first real commit

---
+++ mrtg.rb
@@ -0,0 +1,83 @@
+require ::File.join(File.dirname(__FILE__), "virtual_resources")
+module PoolParty
+  class Plugins
+    
+    # MRTG plugin for poolparty
+    #
+    #
+    # Note: This plugin does *not*
+    #   * control the apache config to view the /var/www/mrtg file
+    #   * add access control for the apache web interface
+    #
+    # You must add these things yourself
+    # 
+    # Helpful Hints:
+    #  To test if snmp is working: 
+    #    snmpwalk -Os -c poolpartycommunity -v 1 localhost system
+    #    snmpwalk -v 1 -c poolpartycommunity localhost IP-MIB::ipAdEntIfIndex
+    # 
+    plugin :mrtg do
+      def enable
+        install
+      end
+            
+      def install
+       unless @installed
+          has_package(:name => "mrtg", :ensures => "installed", :requires => get_package("snmpd"))
+          has_package(:name => "snmpd")
+
+          has_directory("/var/www/mrtg", :requires => get_package("mrtg"))
+
+          configs
+
+          has_exec(:name => "generate_mrtg_index_file", 
+            :command => "/usr/bin/indexmaker --output=/var/www/mrtg/index.html /etc/mrtg.cfg", 
+            :ifnot => "test -f /etc/apache2/conf.d/passenger.conf",
+            :requires => [get_directory("/var/www/mrtg"), get_file("/etc/mrtg.cfg")]
+            )
+
+          has_cron(:name => "Run MRTG", 
+                   :command => "/usr/bin/env LANG=C /usr/bin/mrtg /etc/mrtg.cfg --logging /var/log/mrtg.log", 
+                   :minute => "*/5")
+
+          @installed = true
+        end    
+
+      end
+
+      def base_install        
+        unless @base_install
+          has_exec({:name => "restart-snmpd", :command => "/etc/init.d/snmpd restart", :refreshonly => "true"})
+
+          configs
+          @base_install = true
+        end
+      end
+
+      def configs
+        unless @configs
+
+          has_file({:name => "/etc/mrtg.cfg", 
+                    :template => File.dirname(__FILE__) + "/templates/mrtg.cfg.erb",
+                    :mode => 644})
+
+          has_file({:name => "/etc/default/snmpd", 
+                    :template => File.dirname(__FILE__) + "/templates/snmpd",
+                    :mode => 644})
+
+           has_file({:name => "/etc/snmp/snmpd.conf", 
+                    :template => File.dirname(__FILE__) + "/templates/snmpd.conf",
+                    :mode => 644})
+
+          @configs = true
+        end
+      end
+
+
+      # */10  /usr/lib/sysstat/sa1 -d 1 1
+      # 53 23 /usr/lib/sysstat/sa2 -A
+
+
+    end
+  end
+end
---
+++ virtual_resources.rb
@@ -0,0 +1,4 @@
+module PoolParty
+  # virtual_resource(:standard_monitoring_package) do
+  # end
+end
